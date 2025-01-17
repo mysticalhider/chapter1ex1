a2b79fb08180e9beb99130848ef0b3c7c2722d2e
pjd6878 <pjd6878@HT-0232-04>
2025-01-17 10:29:48 -0500

    Add project files.

---
+++ chapter1ex1/chapter1ex1.cpp
@@ -0,0 +1,19 @@
+/*
+File Name chapter1ex1.cpp
+Programmer: Perrin Dalgleish
+Date 1/17/2025
+Requirements
+take numbers 50 and 100 put into varables
+add the number and place it in a varable named total
+display results
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "What is up bro?\n";
+    return 0;
+}
